a88f7b13b1ebe9bebd7e845f612bda121b327008
whiskas2813 <zincenkoi333@gmail.com>
2025-04-05 16:48:05 +0300

    add hendler

--- bot.py
+++ bot.py
@@ -22,5 +22,9 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
 bot.infinity_polling(none_stop=True)
